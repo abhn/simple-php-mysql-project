4174ee2d0b6b6113cbefb49e66b79611e1f64116
Kunal Rohidas Mhatre <kunalrohidasmhatre@gmail.com>
2015-08-04 20:36:21 +0530

    Create mysql.php

---
+++ mysql.php
@@ -0,0 +1,10 @@
+<?php
+
+$resid=MySQLi_Connect('localhost','root','@connectme','shangout');
+					if(MySQLi_Connect_Errno()) {
+						echo "<tr align='center'> <td colspan='5'> Failed to connect to MySQL </td> </tr>";
+					}
+					else {
+					}
+
+?>
