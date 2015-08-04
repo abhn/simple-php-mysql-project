dcf808bdc68fff85884cfc793300a112a8e7e68c
Kunal Rohidas Mhatre <kunalrohidasmhatre@gmail.com>
2015-08-04 20:33:40 +0530

    Create home.php

---
+++ home.php
@@ -0,0 +1,64 @@
+<!doctype html>
+<html>
+<head>
+	<link rel='stylesheet' href='page_css.css'>
+	<title> Student's Hangout </title>
+</head>
+<body>
+		<table cellpadding='3' cellspacing='3' class='tab_main'>
+			<!--Logo-->
+			<tr>
+				<td  colspan='5'><img src='images/logo.png' height='65%' width='100%' ></td> <!--1350x160-->
+			</tr>
+			<!--Nav_Tabs-->
+			<tr align='center' bgcolor='lightgrey' class='td_bor'>
+				<td width='5%'> <a href='home.php'> Home </a></td>
+				<td width='5%'> <a href='login.php'>Login </a></td>
+				<td width='5%'> <a href='secure_signup.php'>Sign-up </a></td> 
+				<td width='5%'> <a href='contact-us.html'>Contact-Us </a></td>
+				<td width='5%'> <a href='about-us.html'>About-us </a></td>
+			</tr>
+			
+			<tr>
+				<td> <hr> </td> 
+				<td> <hr> </td> 
+				<td> <hr> </td> 
+				<td> <hr> </td> 
+				<td> <hr> </td> 
+			</tr>
+			
+			<tr> 
+				<td width='5%' valign='top'> Objectives of Website:- <br> 
+				<ul type='square'>
+					<li> To send messages to your friends.
+					<li> To find new friends.
+					<li> To update your status.
+					<li> To make your Profile.
+				</ul>
+				</td>
+				
+				<td colspan='4'> 
+				<p> <h3> Hello! Visitor </h3> <br>
+						Welcome to Student's Hangout, A project made for Sending messages and uploading your profile so that your friends can watch out.
+						<br>
+						<h2> We thank following Personalities:-</h2>
+						<ul type='disk'>
+							<li> Project Head:- <a href='http://www.santoshkabirsir.com' target='_blank'> Santosh Kabir Sir </a>(Lecturer)
+							<li> Top Investors:-
+								<ul type='circle'>
+									<li><a href='https://www.google.com' target='_blank'>Google</a>
+									<li><a href='https://www.facebook.com' target='_blank'>Facebook</a>
+									<li><a href='https://www.twitter.com' target='_blank'>Twitter</a>
+								</ul>
+						</ul>
+				</p>	
+				</td>
+				
+			</tr>
+
+		</table>
+			<footer align='center'>
+			&copy; All Rights Reserved.	
+			</footer>
+</body>
+</html>
